151d0f9fed187ad0802e3a92ec0f401d0ef5bf4a
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-01 05:07:56 +0000

    Binary search approach

--- MainProgram.cs
+++ MainProgram.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WelcomeDataStructure
+namespace DataStructureDemo
 {
-    internal class MainProgram
+    public class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data structure sorting Program");
+            BinarySearch dataStructure = new BinarySearch();
+            string[] words = { "decs", "read", "logic", "search" };
+            string data = "search";
+            int result = dataStructure.FindWord(words, data);
+            if (result == -1)
+                Console.WriteLine("Word not present");
+
+            else
+                Console.WriteLine("Word is found at " + result);
+
+
+        }
     }
 }
